bde092626e2a9395ca8b8043d4b38ec1ce8e67d0
Mat911 <45233433+mat976@users.noreply.github.com>
2022-11-16 16:34:52 +0100

    bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-lsimport { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginButtonComponent } from './login-button/login-button.component';
-=======
 import { BodyComponent } from './sidebar/body/body.component';
->>>>>>> 5b06d0c8283c26ec91c2f7b7baa494ca8b057c00
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
+    BodyComponent,
     LoginButtonComponent
-=======
-    BodyComponent
->>>>>>> 5b06d0c8283c26ec91c2f7b7baa494ca8b057c00
   ],
   imports: [
     BrowserModule,

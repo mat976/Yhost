4f4abffdb37da0c33055fbd15c901ab068672e71
Mat911 <45233433+mat976@users.noreply.github.com>
2022-11-16 14:06:02 +0100

    button login base

---
+++ src/app/login-button/login-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-button',
+  templateUrl: './login-button.component.html',
+  styleUrls: ['./login-button.component.css']
+})
+export class ButtonOverviewExample {}
+export class LoginButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+

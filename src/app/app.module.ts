2101d0b3e23caf2ba9118cb55da2a5d0169b9cfe
Mat911 <45233433+mat976@users.noreply.github.com>
2022-11-30 09:11:57 +0100

    reset project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LoginButtonComponent } from './login-button/login-button.component';
-import { BodyComponent } from './sidenav/body/body.component';
-import { CoupensComponent } from './sidenav/coupens/coupens.component';
-import { DashboardComponent } from './sidenav/dashboard/dashboard.component';
-import { MediaComponent } from './sidenav/media/media.component';
-import { PagesComponent } from './sidenav/pages/pages.component';
-import { ProductsComponent } from './sidenav/products/products.component';
-import { SettingsComponent } from './sidenav/settings/settings.component';
-import { SidenavComponent } from './sidenav/sidenav/sidenav.component';
-import { StatisticsComponent } from './sidenav/statistics/statistics.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BodyComponent,
-    SidenavComponent,
-    DashboardComponent,
-    ProductsComponent,
-    StatisticsComponent,
-    CoupensComponent,
-    PagesComponent,
-    MediaComponent,
-    SettingsComponent,
-    BodyComponent,
-    LoginButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

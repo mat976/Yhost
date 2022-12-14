e18a4febe603db161cd4bb3ababef1e3b1c61219
sayk <97113955+saykdoteth@users.noreply.github.com>
2022-12-14 10:02:01 +0100

    component box1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { Box1Component } from './box1/box1.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderNavComponent
+    HeaderNavComponent,
+    Box1Component
   ],
   imports: [
     BrowserModule,
